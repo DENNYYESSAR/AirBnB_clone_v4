740ff5a5cd4d4a3c4d98c3e4748efb0b163fee95
DENNYYESSAR <dennisokeri5@gmail.com>
2024-05-26 18:08:50 +0300

    delete

--- api/v1/views/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python3
-""" Blueprint setup for API v1 views """
-from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
-# Import all views from the package api.v1.views.index
-# PEP8 will complain about this wildcard import, but it's required here
-from api.v1.views.index import *
