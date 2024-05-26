d75aed35b38447245eb101ba406719342dfd7b16
DENNYYESSAR <dennisokeri5@gmail.com>
2024-05-26 18:43:50 +0300

    Not found

--- api/v1/app.py
+++ api/v1/app.py
@@ -4,6 +4,7 @@ from flask import Flask
 from models import storage
 from api.v1.views import app_views
 import os
+from flask import jsonify
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
@@ -13,6 +14,12 @@ app.register_blueprint(app_views)
 def teardown_db(exception):
     """ Close storage session """
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(exception):
+    """ Handle 404 errors with JSON response """
+    return jsonify({"error": "Not found"}), 404
 
 
 if __name__ == "__main__":
