d64a8e51cb83fbcc712923068ea0dd817552a1d5
DENNYYESSAR <dennisokeri5@gmail.com>
2024-05-26 19:02:30 +0300

    State

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -7,3 +7,4 @@ app_views = Blueprint('/api/v1', __name__, url_prefix='/api/v1')
 # Import all views from the package api.v1.views.index
 # PEP8 will complain about this wildcard import, but it's required here
 from api.v1.views.index import *
+from api.v1.views.states import *
